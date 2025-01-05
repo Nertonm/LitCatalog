c7d30416c91f17c1bd9c5b02264dea602021530e
nertonm <thiago2nerton@gmail.com>
2025-01-05 19:23:01 -0300

    starting up

---
+++ src/main/java/br/com/litcatalog/service/ConsumoApi.java
@@ -0,0 +1,10 @@
+package br.com.litcatalog.service;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.io.IOException;
+import java.net.URI;
+
+public class ConsumoApi {
+}
