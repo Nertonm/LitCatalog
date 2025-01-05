f7108a72b3e6ad7dcf291bf1b16f71b941ae4d71
nertonm <thiago2nerton@gmail.com>
2025-01-05 19:08:26 -0300

    starting up

---
+++ litcatalog/src/test/java/br/com/litcatalog/LitcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.litcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
