ca4cf0eabe8337e791550fab7cbad5fd549494e4
nertonm <thiago2nerton@gmail.com>
2025-01-07 02:13:16 -0300

    conectando com o driver do postgres

--- src/main/java/br/com/litcatalog/LitcatalogApplication.java
+++ src/main/java/br/com/litcatalog/LitcatalogApplication.java
@@ -3,11 +3,16 @@ package br.com.litcatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.litcatalog.service.*;
+
 @SpringBootApplication
 public class LitcatalogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LitcatalogApplication.class, args);
+		ConsumoApi consumoApi = new ConsumoApi();
+		System.out.println("Consumindo API...");
+		System.out.println(consumoApi.getDados("https://gutendex.com/books/"));
 	}
 
 }
