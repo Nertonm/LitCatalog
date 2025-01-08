95b06f4d4aba495071a5883edc53b561d7abccc6
nertonm <thiago2nerton@gmail.com>
2025-01-08 14:56:45 -0300

    Changing books models

--- src/main/java/br/com/litcatalog/models/Book.java
+++ src/main/java/br/com/litcatalog/models/Book.java
@@ -1,8 +1,21 @@
 package br.com.litcatalog.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private String title;
+
     private String author;
-    private String languages;
+
+    @Enumerated(EnumType.STRING)
+    private Language languages;
+
     private String downloads;
 }
