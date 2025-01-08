03935f4de14e187897be756e1fa06a64e1c6a920
nertonm <thiago2nerton@gmail.com>
2025-01-08 14:56:28 -0300

    Making language for collum enum in database

---
+++ src/main/java/br/com/litcatalog/models/Language.java
@@ -0,0 +1,13 @@
+package br.com.litcatalog.models;
+
+public enum Language {
+    ENGLISH,
+    SPANISH,
+    FRENCH,
+    GERMAN,
+    CHINESE,
+    JAPANESE,
+    PORTUGUESE,
+    ITALIAN,
+    RUSSIAN
+}
