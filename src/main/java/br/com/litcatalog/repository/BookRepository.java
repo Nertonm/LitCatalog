71807a797708e81ba23e555d1dbcc79b315b1643
nertonm <thiago2nerton@gmail.com>
2025-01-09 19:01:46 -0300

    Updating compatibility

--- src/main/java/br/com/litcatalog/repository/BookRepository.java
+++ src/main/java/br/com/litcatalog/repository/BookRepository.java
@@ -1,7 +1,10 @@
 package br.com.litcatalog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import br.com.litcatalog.models.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    boolean existsByTitle(String title);
 }
