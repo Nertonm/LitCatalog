2828a55e55eae491d01dfca740143792e205040f
nertonm <thiago2nerton@gmail.com>
2025-01-09 15:40:47 -0300

    Adding getters and setters

--- src/main/java/br/com/litcatalog/models/Book.java
+++ src/main/java/br/com/litcatalog/models/Book.java
@@ -9,6 +9,46 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Language getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(Language languages) {
+        this.languages = languages;
+    }
+
+    public String getDownloads() {
+        return downloads;
+    }
+
+    public void setDownloads(String downloads) {
+        this.downloads = downloads;
+    }
+
     @Column(unique = true)
     private String title;
 
