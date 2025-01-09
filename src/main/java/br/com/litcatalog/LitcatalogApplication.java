23c7ab381efc6a2a9c9e48cd01c270372fcd57e4
nertonm <thiago2nerton@gmail.com>
2025-01-09 15:37:16 -0300

    Debugging

--- src/main/java/br/com/litcatalog/LitcatalogApplication.java
+++ src/main/java/br/com/litcatalog/LitcatalogApplication.java
@@ -3,6 +3,7 @@ package br.com.litcatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.litcatalog.models.Book;
 import br.com.litcatalog.service.*;
 
 @SpringBootApplication
@@ -11,9 +12,15 @@ public class LitcatalogApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LitcatalogApplication.class, args);
 		ConsumoApi consumoApi = new ConsumoApi();
+		Book book = new Book();
 		System.out.println("Consumindo API...");
 		BookService bookService = new BookService();
-		System.out.println(bookService.SearchBookbyTitle("Esaú e Jacó"));
+		try {
+			String bookJson = bookService.getBookAsJson("Esaú e Jacó");
+			System.out.println(bookJson);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
