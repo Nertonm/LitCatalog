52c7c4620573d26dc2e89bcd35864acdb78fc457
nertonm <thiago2nerton@gmail.com>
2025-01-10 19:16:28 -0300

    Deleting unused getters and setters

--- src/main/java/br/com/litcatalog/models/Book.java
+++ src/main/java/br/com/litcatalog/models/Book.java
@@ -19,13 +19,6 @@ public class Book {
     private String downloads;
 
     // Getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getTitle() {
         return title;
