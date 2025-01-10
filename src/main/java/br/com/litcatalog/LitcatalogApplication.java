bc0a5c91f37d696c4fa505134ea3622462991493
nertonm <thiago2nerton@gmail.com>
2025-01-10 17:46:53 -0300

    Making app loop

--- src/main/java/br/com/litcatalog/LitcatalogApplication.java
+++ src/main/java/br/com/litcatalog/LitcatalogApplication.java
@@ -1,22 +1,54 @@
 package br.com.litcatalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import br.com.litcatalog.facade.BookFacade;
 
-import br.com.litcatalog.models.Book;
-import br.com.litcatalog.service.*;
-import org.springframework.context.ApplicationContext;
-
-import java.io.IOException;
+import java.util.Scanner;
 
 @SpringBootApplication
-public class LitcatalogApplication {
+public class LitcatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private BookFacade bookFacade;
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(LitcatalogApplication.class, args);
-		BookService bookService = context.getBean(BookService.class);
+		SpringApplication.run(LitcatalogApplication.class, args);
+	}
 
-		System.out.println(bookService.getBookByTitle("Great Expectations").toString());
-		System.out.println(bookService.getBookByTitle("Great Expectations").toString());
+	@Override
+	public void run(String... args) throws Exception {
+		displayMenu();
+	}
+
+	private void displayMenu() {
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("1. Get book by title");
+			System.out.println("2. Get all books");
+			// System.out.println("3. Save book");
+			System.out.println("6. Exit");
+			System.out.print("Enter your choice: ");
+			int choice = scanner.nextInt();
+			scanner.nextLine();
+			switch (choice) {
+				case 1:
+					System.out.print("Enter book title: ");
+					String title = scanner.nextLine();
+					System.out.println(bookFacade.getBookByTitle(title));
+					break;
+				case 2:
+					bookFacade.getAllBooks().forEach(System.out::println);
+					break;
+				case 6:
+					System.out.println("Exiting...");
+					scanner.close();
+					return;
+				default:
+					System.out.println("Invalid choice. Please try again.");
+			}
+		}
 	}
 }
