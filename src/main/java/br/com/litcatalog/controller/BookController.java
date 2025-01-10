ea241981673680e3007b763c7e5d22f14ee2e54c
nertonm <thiago2nerton@gmail.com>
2025-01-09 21:29:58 -0300

    Adding getBookByTitle and getAllBooks methods

--- src/main/java/br/com/litcatalog/controller/BookController.java
+++ src/main/java/br/com/litcatalog/controller/BookController.java
@@ -1,4 +1,30 @@
 package br.com.litcatalog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.litcatalog.models.Book;
+import br.com.litcatalog.service.BookService;
+
+import java.util.List;
+
+@RestController
 public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+    @GetMapping("/title/{title}")
+    public Book getBookByTitle(@PathVariable String title) {
+        return bookService.getBookByTitle(title);
+    }
 }
--- src/main/java/br/com/litcatalog/repository/BookRepository.java
+++ src/main/java/br/com/litcatalog/repository/BookRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import br.com.litcatalog.models.Book;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
     boolean existsByTitle(String title);
+    Optional<Book> findByTitle(String title);
+
 }
