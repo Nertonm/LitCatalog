d0027bec7aaad3044356269dce219017aef4d894
nertonm <thiago2nerton@gmail.com>
2025-01-10 17:18:21 -0300

    New file BookFacade

---
+++ src/main/java/br/com/litcatalog/facade/BookFacade.java
@@ -0,0 +1,35 @@
+package br.com.litcatalog.facade;
+
+import br.com.litcatalog.models.Book;
+import br.com.litcatalog.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class BookFacade {
+
+    @Autowired
+    private BookService bookService;
+
+    public List<String> getAllBooks() {
+       return bookService.getAllBooks().stream()
+               .map(Book::toString)
+               .collect(Collectors.toList());
+    }
+
+    public String getBookByTitle(String title) {
+        return bookService.getBookByTitle(title).toString();
+    }
+
+    public Book saveBook(Book book) {
+        return bookService.saveBook(book);
+    }
+
+    public String getBookAsJson(String title) throws IOException {
+        return bookService.getBookAsJson(title);
+    }
+}
